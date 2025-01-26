b96a2137e6ef93170f419d89c628359d99c6347b
SY-42 <103295003+SY-42@users.noreply.github.com>
2025-01-25 22:39:29 -0800

    Feat(Dark Mode): Add dark mode using JavaScript

---
+++ global.js
@@ -0,0 +1,72 @@
+console.log('IT’S ALIVE!');
+
+function $$(selector, context = document) {
+  return Array.from(context.querySelectorAll(selector));
+}
+
+// let navLinks = $$("nav a")
+// let currentLink = navLinks.find(
+//     (a) => a.host === location.host && a.pathname === location.pathname
+// );
+// currentLink?.classList.add('current');
+
+const ARE_WE_HOME = document.documentElement.classList.contains('home');
+
+let pages = [
+    { url: '', title: 'Home' },
+    { url: 'projects/', title: 'Projects' },
+    { url: 'contact/', title: 'Contact'},
+    { url: 'resume/', title: 'Resume'},
+    { url: 'https://github.com/SY-42', title: 'Github'}
+  ];
+  
+let nav = document.createElement('nav');
+document.body.prepend(nav);
+  
+for (let p of pages) {
+    let url = p.url;
+    url = !ARE_WE_HOME && !url.startsWith('http') ? '../' + url : url;
+    let title = p.title;
+    let a = document.createElement('a');
+    a.href = url;
+    a.textContent = title;
+    a.classList.toggle(
+        'current',
+        a.host === location.host && a.pathname === location.pathname
+    );
+    if(a.host != location.host){
+        a.target = "_blank"
+    }
+    nav.append(a);
+
+}
+
+document.body.insertAdjacentHTML(
+  'afterbegin',
+  `
+	<label class="color-scheme">
+		Theme:
+		<select>
+      <option value='light dark'>Automatic</option>
+			<option value='light'>Light</option>
+      <option value='dark'>Dark</option>
+		</select>
+	</label>`
+);
+
+const setColor = (colorScheme) => {
+  document.documentElement.style.setProperty('color-scheme', colorScheme);
+}
+
+const select = document.querySelector('select');
+
+select.addEventListener('input', function (event) {
+  console.log('color scheme changed to', event.target.value);
+  setColor(event.target.value);
+  localStorage.colorScheme = event.target.value;
+});
+
+if("colorScheme" in localStorage){
+  setColor(localStorage.colorScheme);
+  select.value = localStorage.colorScheme;
+}
